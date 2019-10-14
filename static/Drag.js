1e1f9f288fe9775461a446c857a231a80953d183
smoriya1 <2271595m@student.gla.ac.uk>
2019-10-14 01:15:40 +0100

    Interact js test changes

--- static/Drag.js
+++ static/Drag.js
@@ -1,37 +1,39 @@
 interact('.draggable')
   .draggable({
-    max: Infinity,
-
     onmove: function(event) {
-      var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x'))||0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y'))||0) + event.dy;
-      target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-      target.setAttribute('data-x',x);
-      target.setAttribute('data-y',y);
-    },
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
+      var target = event.target;
+      var x = target.getAttribute('data-x');
+      var y = target.getAttribute('data-y');
+      var initX = parseFloat(x)||0;
+      var initY = parseFloat(y)||0;
+      var deltaX = event.dx;
+      var deltaY = event.dy;
+      var newX = initX + deltaX;
+      var newY = initY + deltaY;
+      target.style.transform = `translate(${newX}px, ${newY}px)`;
+      target.setAttribute('data-x', newX);
+      target.setAttribute('data-y', newY);
     }
   })
-  .restrict({
-    drag: "parent",
-    endOnly: true,
-    elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-  })
-  .snap({mode: 'path', paths: [function(x,y) {
-    var dropzoneRect = interact.getElementRect(document.getElementbyId('dropzone'));
-    if(x >= dropzoneRect.left && x <= dropzoneRect.right && y >= dropzoneRect.top &&
-    y <= dropzoneRect.bottom) {
-      return {
-        x: dropzoneRect.left + dropzoneRect.width/2,
-        y: dropzoneRect.left + dropzoneRect.height/2,
-        range: Infinity
+
+  interact('.dropoff')
+    .dropzone({
+      accept: '.draggable',
+      overlap: 1,
+      ondropactivate: function (event) {
+        event.target.classList.add('dragging')
+      },
+      ondropdeactivate: function (event) {
+        event.target.classList.remove('dragging')
+        event.target.classList.remove('cannot-drop')
+        event.target.classList.remove('can-drop')
+      },
+      ondragenter: function (event) {
+        event.target.classList.remove('cannot-drop')
+        event.target.classList.add('can-drop')
+      },
+      ondragleave: function (event) {
+        event.target.classList.remove('can-drop')
+        event.target.classList.add('cannnot-drop')
       }
-    }
-    return {x:x, y:y};
-  }],
-  range: Infinity,
-  elementOrigin: {x:0.5,y:0.5},
-  endOnly: true
-});
+    })
